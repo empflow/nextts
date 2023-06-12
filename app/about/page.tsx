08506931e1bf0a4dbc8ef35d1e22cb2a8b4691b9
Matvey <morozovmatvej75@gmail.com>
2023-06-12 10:44:02 +0300

    use a custom self-hosted font

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,11 @@
-"use client"
-
-import { Metadata } from "next";
 import Link from "next/link";
 import { FC } from "react";
 
 interface IAboutProps {
-
+  
 }
 
-// export const metadata: Metadata = {
-//   title: "About page",
-//   description: "This page is created by https://github.com/empflow"
-// }
-
 const About: FC<IAboutProps> = () => {
-  function crashApp() {
-  }
-
   return (
     <>
     <h1>about page</h1>
