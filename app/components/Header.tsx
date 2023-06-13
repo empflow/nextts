f98e053d39590bc402be8cfeea9e8b5561b50e51
Matvey <morozovmatvej75@gmail.com>
2023-06-13 19:37:28 +0300

    add header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,24 @@
-export default function Header() {
+import Link from "next/link";
+
+export default function Header()     {
   return (
-    <header>
-      
+    <header className="gap-6 p-5 backdrop:blur-3xl bg-black/75 backdrop-blur-xl sticky top-0 w-full border-b border-b-white/20">
+      <div className="max-w-6xl flex items-center justify-between m-auto overflow-auto">
+        <h3 className="text-3xl font-semibold">Logo</h3>
+        <nav className="">
+          <ul className="flex gap-8">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/blog">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
