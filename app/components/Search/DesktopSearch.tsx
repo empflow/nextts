8a9860f11c5616861af29606076c6bb90844795f
Matvey <morozovmatvej75@gmail.com>
2023-06-16 17:41:17 +0300

    add search icon in desktop search bar

--- app/components/Search/DesktopSearch.tsx
+++ app/components/Search/DesktopSearch.tsx
@@ -1,3 +1,4 @@
+import SearchIcon from "@/app/icons/Search";
 import { Dispatch, FormEvent, SetStateAction } from "react";
 
 interface DesktopSearchProps {
@@ -13,14 +14,27 @@ export default function DesktopSearch({
 }: DesktopSearchProps) {
   return (
     <div className="hidden md:block">
-      <form className="max-w-[250px] flex-grow" onSubmit={onSubmit}>
+      <form
+        className="flex max-w-[250px] flex-grow items-stretch"
+        onSubmit={onSubmit}
+      >
         <input
           type="text"
           placeholder="Search..."
-          className="w-full rounded bg-c-gray-200 px-3 py-1 text-black duration-200 placeholder:font-light placeholder:text-c-gray-800 dark:bg-zinc-800 dark:text-white"
+          className="w-full rounded rounded-r-none border border-transparent bg-c-gray-200 px-3 py-1 text-black placeholder:font-light placeholder:text-c-gray-800 focus:border-blue-500 dark:bg-c-gray-1000 dark:text-white dark:focus:border-blue-900"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+        <button
+          type="submit"
+          className="flex items-center justify-center rounded-r border-l border-c-gray-600 bg-c-gray-200  p-[5px] dark:border-zinc-600 dark:bg-c-gray-1000"
+          title="Search"
+        >
+          <SearchIcon
+            pxSize={24}
+            className="fill-c-gray-1000 dark:fill-c-gray-500"
+          />
+        </button>
       </form>
     </div>
   );
