3003e54b5f523f597d376112aff68541d59f8cdb
Matvey <morozovmatvej75@gmail.com>
2023-06-17 17:03:45 +0300

    remove dead link from header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -18,9 +18,6 @@ export default function Header() {
             <li>
               <Link href="/about">About</Link>
             </li>
-            <li>
-              <Link href="/blog">Blog</Link>
-            </li>
           </ul>
         </nav>
 
