3efc5224a74552ec9c947e9136deb51fb22d45d1
Matvey <morozovmatvej75@gmail.com>
2023-06-17 17:08:09 +0300

    improve home page & footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
     <>
-      <img
-        src="https://i.ytimg.com/vi/843nec-IvW0/hqdefault_1054800.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8v6VL__El534ASr3Uetb633I4Yg"
-        alt="video thumbnail"
-        style={{ minWidth: "100%" }}
-      />
-      <div className="min-h-screen"></div>
+      <h1 className="text-xl font-semibold">Try searching!!!</h1>
     </>
   );
 }
